19c45a2a9e19419bff05c133eaaf0df7514a0ff9
Eun Mi Kim <koreaeunmi@gmail.com>
2026-02-10 11:39:43 -0500

    Initial Commit

---
+++ M4Lotto_Kim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M4Lotto_Kim
+//
+//  Created by Eun Mi Kim on 2/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
